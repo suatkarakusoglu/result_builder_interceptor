0d0d30c23a07bbf9e56437148f19ca34951edc33
Suat <suat.karakusoglu@d-teknoloji.com.tr>
2021-09-20 15:15:00 +0300

    Initial Commit

---
+++ Builderli/BuilderliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuilderliApp.swift
+//  Builderli
+//
+//  Created by Suat Karakusoglu (Dogus Teknoloji) on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct BuilderliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
